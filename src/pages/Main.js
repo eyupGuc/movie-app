707d36012c62a314c949ee0c5a8928b164e1676a
eyupGuc <meydanguresi@gmail.com>
2022-11-05 21:27:35 +0300

    move

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 
-const Main=()=>{
-return (
+const Main = () => {
+  const [data, setData] = useState();
+  const api = "f00e39d798c8c480b88ab58980ae237d";
 
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${api}&query=Batman`;
+//   const url2 = `https://api.themoviedb.org/3/discover/movie?api_key=${api}`
+
+  const getMovie = async () => {
+    const {res}  = await axios( url);
+    console.log(res)
+    setData(res)
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  console.log(data);
+  return (
     <>
-    <div>main</div>
+      <div>main</div>
     </>
-)
-
-
-}
+  );
+};
 
 export default Main;
