5580bf121c7249cbaae6955eac856181bddb3de2
eyupGuc <meydanguresi@gmail.com>
2022-11-07 01:19:55 +0300

    move

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,54 +3,53 @@ import { useEffect, useState } from "react";
 
 import Search from "../components/Search";
 
+
 const Main = () => {
   const [defaultMovie, setDefaultMovie] = useState();
   const [buttonSearch, setButtonSearch] = useState(false);
-  const [inputSearch, setInputSearch] = useState();
+  const[inputSearch,setInputSearch]=useState();
   const api = "f00e39d798c8c480b88ab58980ae237d";
 
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${api}&query=${inputSearch}`;
   const url2 = `https://api.themoviedb.org/3/discover/movie?api_key=${api}`;
 
   const getMovie = async () => {
-    console.log(buttonSearch);
+    console.log(buttonSearch)
     const res = await axios(buttonSearch ? url : url2);
     console.log(res.data.results);
     setDefaultMovie(res.data.results);
   };
   useEffect(() => {
     getMovie();
-   
   }, []);
 
   // console.log(data);
-  console.log(buttonSearch);
-  console.log(inputSearch);
-
+  console.log(buttonSearch)
+  console.log(inputSearch)
+  
   return (
     <div>
       <div>
-        <Search
-          setButtonSearch={setButtonSearch}
-          setInputSearch={setInputSearch}
-        />
+     
+        <Search setButtonSearch={setButtonSearch} setInputSearch={setInputSearch} />
       </div>
+      
+        {defaultMovie?.map((item) => {
+          // console.log(item);
+          const { poster_path, vote_average, title, overview, id } = item;
 
-      {defaultMovie?.map((item) => {
-        // console.log(item);
-        const { poster_path, vote_average, title, overview, id } = item;
-
-        return (
-          <div key={id}>
-            <h2>{title}</h2>
-            <img
-              src={`https://image.tmdb.org/t/p/w1280${poster_path}`}
-              width="300px"
-              alt=""
-            />
-          </div>
-        );
-      })}
+          return (
+            <div key={id}>
+              <h2>{title}</h2>
+              <img
+                src={`https://image.tmdb.org/t/p/w1280${poster_path}`}
+                width="300px"
+                alt=""
+              />
+            </div>
+          );
+        })}
+    
     </div>
   );
 };
